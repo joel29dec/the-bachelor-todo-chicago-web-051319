d6ee5ca00ac0a6349aa54a3935591fcf36d85ddb
Joel S <joel29dec@gmail.com>
2019-04-10 19:59:52 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,7 +11,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  data[season].each do |k|
+  data.each do |k|
     k.each do |key, value|
       if value == occupation
         binding.pry
